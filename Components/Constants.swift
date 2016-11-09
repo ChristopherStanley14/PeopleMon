e4288bdf9f818649ca27bce168fb14f702b06cc7
ChristopherStanley14 <christopherstanley@ChristopherStanleyMBP.local>
2016-11-08 20:29:10 -0500

    Updated constants and app delegate

--- Components/Constants.swift
+++ Components/Constants.swift
@@ -39,96 +39,20 @@ struct Constants {
         static let avatarBase64 = "AvatarBase64"
         static let lastCheckInLongitude = "LastCheckInLongitude"
         static let lastCheckInLatitude = "LastCheckInLatitude"
-        static let lastCheckInDateTime = "LastCheckInDateTime"
-        static let oldPassword = "OldPassword"
-        static let newPassword = "NewPassword"
-        static let confirmPassword = "ConfirmPassword"
         static let password = "Password"
         static let apiKey = "ApiKey"
         static let grantType = "grant_type"
+        static let userName = "userName"
     }
     
     struct User {
         static let userId = "UserId"
         static let userName = "UserName"
-        static let avatarBase64 = "AvatarBase64"
         static let longitude = "Longitude"
         static let latitude = "Latitude"
+        static let avatarBase64 = "AvatarBase64"
+        static let created = "Created"
         static let caughtUserId = "CaughtUserId"
-        static let radiusInMeters = "RadiusInMeters"
-        static let conversationId = "ConversationId"
-        static let recipientId = "RecipientId"
-        static let recipientName = "RecipientName"
-        static let lastMessage = "LastMessage"
-        static let messageCount = "MessageCount"
-        static let senderId = "SenderId"
-        static let senderName = "SenderName"
-        static let recipientAvatarBase64 = "RecipientAvatarBase64"
-        static let senderAvatarBase64 = "SenderAvatarBase64"
-        static let pageSize = "PageSize"
-        static let pageNumber = "PageNumber"
-        static let id = "Id"
-        static let message = "Message"
+        static let radiusInMeters = "radiusInMeters"
     }
-    
-    
-//    // Step 9: BudgetUser Constants
-//    struct BudgetUser {
-//        static let id = "id"
-//        static let email = "email"
-//        static let username = "username"
-//        static let password = "password"
-//        static let token = "token"
-//        static let expirationDate = "expiration"
-//    }
-//    
-//    // Step 10: Category Constants
-//    struct Category {
-//        static let id = "id"
-//        static let name = "name"
-//        static let categoryInfo = "category_info"
-//        static let startDate = "start_date"
-//        static let endDate = "end_date"
-//        static let user = "user"
-//        static let amount = "amount"
-//        
-//        static let month = "month"
-//        static let day = "day"
-//        static let year = "year"
-//    }
-//    
-//    // Step 11: Expense Constants
-//    struct Expense {
-//        static let id = "id"
-//        static let amount = "amount"
-//        static let category = "category"
-//        static let date = "date"
-//        static let note = "note"
-//        static let categoryId = "categoryId"
-//        static let categoryName = "categoryName"
-//        
-//       
-//        
-//        // Step 19: Add month/year
-//        static let month = "month"
-//        static let year = "year"
-//    }
-}
-
-// MARK: - Colors
-// Step 14: UIColor extension and
-extension UIColor {
-    public class func rgba(red: NSInteger, green: NSInteger, blue: NSInteger, alpha: Float = 1.0) -> UIColor {
-        return UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: CGFloat(alpha))
-    }
-}
-
-struct ColorPalette {
-    static let PositiveColor = UIColor.rgba(red: 15, green: 181, blue: 46)
-    static let NegativeColor = UIColor.rgba(red: 219, green: 31, blue: 31)
-    static let BlueColor = UIColor.rgba(red: 12, green: 35, blue: 64)
-    static let GoldColor = UIColor.rgba(red: 201, green: 151, blue: 0)
-    static let calendarCellColor = UIColor.rgba(red: 0, green: 0, blue: 0, alpha: 0.1)
-    static let calendarTodayColor = UIColor.rgba(red: 12, green: 35, blue: 64, alpha: 0.3)
-    static let calendarBorderColor = UIColor.rgba(red: 12, green: 35, blue: 64, alpha: 0.8)
 }
